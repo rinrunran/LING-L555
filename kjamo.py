fe2bcb51cabf35095a3aaca4bcea2e702b86207a
rinrunran <134823630+rinrunran@users.noreply.github.com>
2023-12-15 00:18:19 -0500

    Update kjamo.py

--- kjamo.py
+++ kjamo.py
@@ -4,4 +4,8 @@ import jamo
 
 from jamo import hangul_to_jamo
 
+#this file was practice mainly for how to import/export jamo and use the functions within the library
+#so, this file was edited and deleted several of times
 hangul = hangul_to_jamo(sys.stdin.readline())
+
+#there is no relevance, really, for this file with my project
