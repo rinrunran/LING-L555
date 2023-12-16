81f2ef345f468f95ccff4a12efe5e1ade5f599ec
rinrunran <134823630+rinrunran@users.noreply.github.com>
2023-12-15 20:41:17 -0500

    Update train.py

--- practicals/train.py
+++ practicals/train.py
@@ -1,43 +1,31 @@
 # Open the wiki.conllu file in read mode
 fd = open('wiki.conllu', 'r')
 
-#print(#P	Count	Tag	Form)
+#print(#P Count Tag Form)
 
-m={}
-n={}
+m = {}
+ = {}
 
+#for each instance in the imported text
 for line in fd.readlines():
-	# Strip the newline
+	#strip newlines
 	line = line.strip('\n')
-	# if there is no tab character, skip the line
+	#skip the line if there isn' any tab character
 	if '\t' not in line:
 		continue
+	#define the row, form, and tag variables
 	row = line.split()
+	#form is defined as being the value in the 'second' position in the row, which is the tokenized word
 	form = row[1]
+	#defines the tag variable as being in the third position of the row
 	tag = row[3]
+	#create a function to add form to the dictionary 'm' if it is not there already and set an empty value for 
 	if form not in m:
 		m[form]={}
+	#create a function to set tag to the form 
 	if tag not in m[form]:
 		m[form][tag]=0
 	m[form][tag] += 1
 	if tag not in n:
 		n[tag]=0
 	n[tag]+= 1
-
-totaln = 0
-
-for tag in n:
-	totaln += n[tag]
-
-for tag in n:
-	probn=n[tag]/totaln
-	print('%.2f\t%s\t%s\t%s'%(probn, n[tag], tag, form))
-
-
-for form in m:
-	tot=0
-	for tag in m[form]:
-		tot += m[form][tag]
-	for tag in m[form]:
-		prob=m[form][tag]/tot
-		print('%.2f\t%.2f\t%s\t%s'%(prob, tot, tag, form))
